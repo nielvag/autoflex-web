df3a88d0468c6e357be65de3675232b92125d9f3
Nielson Vágno <nielsonvagno@gmail.com>
2026-02-14 09:34:18 -0300

    feat: configure redux store

---
+++ src/stores/api/baseApi.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000",
+  }),
+  tagTypes: [
+    "Products",
+    "RawMaterials",
+    "ProductRawMaterials",
+    "ProductionPlan",
+  ],
+  endpoints: () => ({}),
+});
